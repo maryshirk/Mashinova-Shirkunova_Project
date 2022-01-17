684117ab40869c4c414d8ebeec0350628eac9d75
newsedative <knowslana@gmail.com>
2022-01-17 19:26:19 +0300

    change

--- main_1.py
+++ main_1.py
@@ -568,64 +568,62 @@ def main():
         game()
 
 
+class Let(pygame.sprite.Sprite):
+    def __init__(self, x, speed, filename):
+        pygame.sprite.Sprite.__init__(self)
+        self.image = pygame.image.load(filename).convert_alpha()
+        self.image = pygame.transform.scale(self.image, (200, 200))
+        self.rect = self.image.get_rect(center=(x, random.randint(0, 500)))
+        self.speed = speed
+
+    def update(self, *args):
+        if self.rect.x > args[0]:
+            self.rect.x -= self.speed
+        else:
+            self.kill()
+
+
 class SecondLevel:
-    def __init__(self, font):
-        self.click = False
-        self.font = font
-        self.background = pygame.image.load('assets/water_bg1.jpg')
+    def __init__(self):
         self.water = pygame.image.load('assets/water_texture.png')
         self.background1 = pygame.image.load('assets/list.jpg')
-
-    def start_the_game(self):
-        while True:
-            screen.fill((0, 0, 0))
-            screen.blit(self.background1, (0, 0))
-            mx, my = pygame.mouse.get_pos()
-            button_1 = pygame.Rect(350, 250, 200, 50)
-            if button_1.collidepoint((mx, my)):
-                if self.click:
-                    self.game()
-            pygame.draw.rect(screen, (255, 0, 0), button_1)
-            self.draw_text('start', self.font, (255, 255, 255), screen, 415, 260)
-
-            self.click = False
-            for event in pygame.event.get():
-                if event.type == QUIT:
-                    pygame.quit()
-                    sys.exit()
-                if event.type == KEYDOWN:
-                    if event.key == K_ESCAPE:
-                        pygame.quit()
-                        sys.exit()
-                if event.type == MOUSEBUTTONDOWN:
-                    if event.button == 1:
-                        self.click = True
-
-            pygame.display.update()
-            mainClock.tick(60)
-
-    def draw_text(self, text, font, color, surface, x, y):
-        textobj = font.render(text, 1, color)
-        textrect = textobj.get_rect()
-        textrect.topleft = (x, y)
-        surface.blit(textobj, textrect)
+        self.ship = pygame.image.load('assets/ship_png.png')
+        self.ship = pygame.transform.scale(self.ship, (240, 150))
+        self.shipYpos = 0
+        self.ship_rect = self.ship.get_rect(center=(10, self.shipYpos))
+        pygame.time.set_timer(pygame.USEREVENT, 4000)
 
     def game(self):
         running = True
         self.waterXpos = 0
+        gravity = 2
+        W = -200
+        group = pygame.sprite.Group()
+        group.add(Let(800, random.randint(1, 4), 'assets/island.png'))
+        print(len(group))
 
         def draw_water():
-            screen.blit(self.water, (self.waterXpos, 450))
-            screen.blit(self.water, (self.waterXpos + 288, 450))
+            screen.blit(self.water, (self.waterXpos, 445))
+            screen.blit(self.water, (self.waterXpos + 288, 445))
 
         while running:
             screen.fill((0, 0, 0))
-            screen.blit(self.background, (0, 0))
+            screen.blit(self.background1, (0, 0))
+            screen.blit(self.ship, (10, self.shipYpos))
+            group.draw(screen)
             for event in pygame.event.get():
                 if event.type == QUIT:
                     pygame.quit()
                     sys.exit()
-
+                if event.type == pygame.KEYDOWN:
+                    if event.key == pygame.K_SPACE:
+                        self.shipYpos -= 70
+                if event.type == pygame.USEREVENT:
+                    group.add(Let(900, random.randint(1, 4), 'assets/island.png'))
+            if self.shipYpos > 310:
+                self.shipYpos = 320
+            else:
+                self.shipYpos += gravity
             self.waterXpos -= 5  # скорость текстуры воды
             draw_water()
             if self.waterXpos <= -288:
@@ -633,7 +631,7 @@ class SecondLevel:
 
             pygame.display.update()
             mainClock.tick(60)
-
+            group.update(W)
 
 
 def game():
@@ -712,7 +710,7 @@ class Menu2:
                 if next_button.collidepoint((mx, my)):
                     if self.click:
                         level = SecondLevel(font)
-                        level.start_the_game()
+                        level.game()
                 draw_text('Вы выиграли', self.font, (255, 0, 0), screen, 350, 190)
                 draw_text('к уровню', self.font, (255, 0, 0), screen, 360, 255)
                 pygame.draw.rect(screen, (255, 0, 0), next_button, 2)
